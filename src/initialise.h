5650d977a3beb0db39bb07b62170f1c08c24eafb
Peter Tasker <petertasker05@gmail.com>
2025-02-11 17:26:40 +0000

    remove redundancy from header

--- src/initialise.h
+++ src/initialise.h
@@ -1,7 +1,5 @@
 char *createBuffer();
 char *saveInitialDirectory();
 char *initialiseDirectory();
-char **initialiseAliasNames();
-char **initialiseAliasCommands();
 char *concatFilePath(char *);
 void loadIntoHistory(DNode**, char **tokens);
